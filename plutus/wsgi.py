dcda9cff40410cec6c6fdc9c6b19a0314e928796
Amir Amiri <amiriamirr533@gmail.com>
2025-10-17 22:15:43 -0600

    service updated

--- plutus/wsgi.py
+++ plutus/wsgi.py
@@ -11,7 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from whitenoise import WhiteNoise
 from django.conf import settings
 
 
@@ -19,5 +18,3 @@ from django.conf import settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'plutus.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=settings.STATIC_ROOT)
-application.add_files(settings.STATIC_ROOT)
