36d0d0103577cbe71cd270f0fe8d3797dabe4f3d
Amir Amiri <amiriamirr533@gmail.com>
2025-10-17 22:26:08 -0600

    service updated

--- apps/telegram_bot/tasks.py
+++ apps/telegram_bot/tasks.py
@@ -27,19 +27,19 @@ def get_bot():
     return _bot
 
 
-@shared_task()
-def send_telegram_alert(strategy_alert_id, log):
-    try:
-        logger.info(f'sending telegram alert {strategy_alert_id} - {log}')
-        alert = StrategyAlert.objects.get(id=strategy_alert_id, is_enable=True)
-        bot = get_bot()
-        if not bot:
-            return
-        bot.send_message(
-            text=log,
-            chat_id=alert.extra_data['telegram_user_id'],
-            reply_markup=buttons.start()
-        )
-        AlertLog.objects.create(strategy_alert=alert, log=log)
-    except Exception as e:
-        logger.error(f'sending telegram alert error: {e}')
+# @shared_task()
+# def send_telegram_alert(strategy_alert_id, log):
+#     try:
+#         logger.info(f'sending telegram alert {strategy_alert_id} - {log}')
+#         alert = StrategyAlert.objects.get(id=strategy_alert_id, is_enable=True)
+#         bot = get_bot()
+#         if not bot:
+#             return
+#         bot.send_message(
+#             text=log,
+#             chat_id=alert.extra_data['telegram_user_id'],
+#             reply_markup=buttons.start()
+#         )
+#         AlertLog.objects.create(strategy_alert=alert, log=log)
+#     except Exception as e:
+#         logger.error(f'sending telegram alert error: {e}')
